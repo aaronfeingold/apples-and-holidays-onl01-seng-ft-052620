219f45f7121e9384f1f161f687faffbca01491cf
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-08 20:26:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,13 +72,11 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-  holiday_hash.each do |season, holidays|
-    holidays.each do |holiday, supply|  
-      supply.include 
-    end 
-  end 
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      holiday if supplies.include?("BBQ")
+    end
+  end.flatten.compact
 end
 
 
